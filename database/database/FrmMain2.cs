d71d5a9ec957b761f03120d07f3f647ef4338772
Noselycha <noselychasoriton04@gmail.com>
2024-09-24 14:56:53 +0700

    Add files via upload

---
+++ database/database/FrmMain2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace database
+{
+    public partial class FrmMain2 : Form
+    {
+        public FrmMain2()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain2_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
